a461c64a245e5949485ffb814c01e988e07c0776
Ta Minh Duc <minhduc@gaincity.com>
2022-12-28 17:18:01 +0800

    Adding binary file to execute

--- src/cli/install-crontab.sh
+++ src/cli/install-crontab.sh
@@ -1,3 +1,3 @@
 ROOT=$(readlink -f "$( dirname -- "$0")/../../");
-crontab -l | { cat; echo "*/5 * * * * cd $ROOT; $(which node) ./src/index.js >> $ROOT/run.log"; } | crontab -
-crontab -l | { cat; echo "@reboot cd $ROOT; $(which node) ./src/index.js >> $ROOT/run.log"; } | crontab -
+crontab -l | { cat; echo "*/5 * * * * cd $ROOT; $ROOT/cloudflare-dns >> $ROOT/run.log"; } | crontab -
+crontab -l | { cat; echo "@reboot cd $ROOT; $ROOT/cloudflare-dns >> $ROOT/run.log"; } | crontab -
